e394869462e73c5d53b47b59bb002b3dd60966ab
HerbertDaca <zielarz.x@gmail.com>
2023-02-21 23:56:04 +0100

    usunąłem niepotrzebne printy

--- src/main.py
+++ src/main.py
@@ -19,10 +19,6 @@ gpd_data = library.read_data(args.gpd, 3)
 pop_data = library.read_data(args.population, 3)
 emi_data = library.read_data(args.emission, 0)
 
-print(library.get_min_max_years_from_row(gpd_data))
-print(library.get_min_max_years_from_row(pop_data))
-print(library.get_min_max_years_from_column(emi_data))
-
 print(library.get_year_range(gdp_data=gpd_data, pop_data=pop_data, emi_data=emi_data))
 
 # print(library.get_min_max_years_from_row(gpd_data))
