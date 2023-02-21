bf1e65da9ce6daf5212365e4e487487ffbc9551e
HerbertDaca <zielarz.x@gmail.com>
2023-02-21 21:53:23 +0100

    dodałem wykrywanie zakresu lat

--- src/library.py
+++ src/library.py
@@ -5,10 +5,13 @@ def read_data(filename, skiprows):
     return pandas.read_csv(filename, skiprows=skiprows)
 
 def get_min_max_years_from_row(data):
-    return[1, 2]
+    return [int(data.columns[4]), int(data.columns[-2])]
 
 def get_min_max_years_from_column(data):
-    return[1, 2]
+    return[data['Year'].min(), data['Year'].max()]
 
-def adjust_year_range(min_year_1, max_year_1, min_year_2, max_year_2, min_year_3, max_year_3):
-    return[max(min_year_1, min_year_2, min_year_3), min(max_year_1, max_year_2, max_year_3)]
+def get_year_range(gdp_data, pop_data, emi_data):
+    [gdp_min, gdp_max] = get_min_max_years_from_row(gdp_data)
+    [pop_min, pop_max] = get_min_max_years_from_row(pop_data)
+    [emi_min, emi_max] = get_min_max_years_from_column(emi_data)
+    return[max(gdp_min, pop_min, emi_min), min(gdp_max, pop_max, emi_max)]
--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import argparse
+
+import pandas
+
 import library
 
 
@@ -6,16 +9,25 @@ parser = argparse.ArgumentParser(description='zaleznosci miedzy dochodami panstw
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('gpd', type=str)
-parser.add_argument('citizens', type=str)
+parser.add_argument('population', type=str)
 parser.add_argument('emission', type=str)
 
 args = parser.parse_args()
 
-gpd_data = library.read_data(args.gpd, 2)
-cit_data = library.read_data(args.citizens, 2)
+
+gpd_data = library.read_data(args.gpd, 3)
+pop_data = library.read_data(args.population, 3)
 emi_data = library.read_data(args.emission, 0)
 
+print(library.get_min_max_years_from_row(gpd_data))
+print(library.get_min_max_years_from_row(pop_data))
+print(library.get_min_max_years_from_column(emi_data))
+
+print(library.get_year_range(gdp_data=gpd_data, pop_data=pop_data, emi_data=emi_data))
+
+# print(library.get_min_max_years_from_row(gpd_data))
+# print(library.get_min_max_years_from_row(cit_data))
+# print(library.get_min_max_years_from_column(emi_data))
 
 # data.columns = data.columns.astype("str")
 # pandas.options.display.float_format = '{:.2f}'.format
-# print(data)
