504bd8fafc178bcbdf7096c18310646dc45286d7
HerbertDaca <zielarz.x@gmail.com>
2023-02-22 21:47:44 +0100

    kontynuowałem pisanie
    jp

--- src/library.py
+++ src/library.py
@@ -1,4 +1,5 @@
 import pandas
+import numpy
 
 
 def read_data(filename, skiprows):
@@ -10,8 +11,38 @@ def get_min_max_years_from_row(data):
 def get_min_max_years_from_column(data):
     return[data['Year'].min(), data['Year'].max()]
 
-def get_year_range(gdp_data, pop_data, emi_data):
+def get_years_array(gdp_data, pop_data, emi_data):
     [gdp_min, gdp_max] = get_min_max_years_from_row(gdp_data)
     [pop_min, pop_max] = get_min_max_years_from_row(pop_data)
     [emi_min, emi_max] = get_min_max_years_from_column(emi_data)
-    return[max(gdp_min, pop_min, emi_min), min(gdp_max, pop_max, emi_max)]
+    return list(range(max(gdp_min, pop_min, emi_min), min(gdp_max, pop_max, emi_max)))
+
+def get_merged_data(gdp_data, pop_data, emi_data, years, countries):
+    for year in years:
+        for country in countries:
+            gdp = get_gdp_or_population(year, country, gdp_data, 'GDP')
+            pop = get_gdp_or_population(year, country, pop_data, 'population')
+            new_row = [year, country, pop, gdp, gdp/pop, '', '']
+            if gdp.count() != 0:
+                print(gdp.item)
+                print(float(gdp))
+            merged_array = numpy.append(merged_array, [year, country, ])
+    merged_dataframe = 0
+    return merged_dataframe
+
+def get_gdp_or_population(year, country, data, data_caption):
+    cell = data.loc[data['Country Name'] == country, str(year)]
+    if cell.count() == 0:
+        print(country + ' could not be found in ' + data_caption + ' data.')
+        return numpy.Nan
+    return float(cell.item)
+
+def get_emission(year, country, data):
+    return 0
+
+def get_countries_array(gdp_data, pop_data, emi_data):
+    gdp_countries = gdp_data['Country Name'].to_numpy().astype(str)
+    pop_countries = pop_data['Country Name'].to_numpy().astype(str)
+    emi_countries = numpy.unique(numpy.array(emi_data['Country']))
+    countries = numpy.unique(numpy.hstack((gdp_countries, pop_countries, emi_countries)))
+    return countries
--- src/main.py
+++ src/main.py
@@ -1,29 +1,33 @@
 import argparse
 
-import pandas
-
 import library
 
+import numpy
 
-parser = argparse.ArgumentParser(description='zaleznosci miedzy dochodami panstw, liczbami mieszkancow i emisja CO2',
+
+parser = argparse.ArgumentParser(description='dependencies between GDP, population and CO2 emission',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('gpd', type=str)
+parser.add_argument('gdp', type=str)
 parser.add_argument('population', type=str)
 parser.add_argument('emission', type=str)
 
 args = parser.parse_args()
 
-
-gpd_data = library.read_data(args.gpd, 3)
+gdp_data = library.read_data(args.gdp, 3)
+gdp_data['Country Name'] = gdp_data['Country Name'].str.upper()
 pop_data = library.read_data(args.population, 3)
+pop_data['Country Name'] = pop_data['Country Name'].str.upper()
 emi_data = library.read_data(args.emission, 0)
 
-print(library.get_year_range(gdp_data=gpd_data, pop_data=pop_data, emi_data=emi_data))
+years = library.get_years_array(gdp_data, pop_data, emi_data)
+print(years)
+countries = library.get_countries_array(gdp_data, pop_data, emi_data)
+print(countries)
 
-# print(library.get_min_max_years_from_row(gpd_data))
-# print(library.get_min_max_years_from_row(cit_data))
-# print(library.get_min_max_years_from_column(emi_data))
+merged_array = numpy.array([])
+print(gdp_data)
+
 
 # data.columns = data.columns.astype("str")
 # pandas.options.display.float_format = '{:.2f}'.format
